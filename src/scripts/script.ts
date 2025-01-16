f288660a75f0e4288e13fbdde5cf68b0644aafcf
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-01-16 21:09:07 +0530

    practice

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -21,27 +21,3 @@ and it should
 this is just for github contributions
 
 */
-
-/*
-
-This is a script file for all typescript notes, all the practice scipts will be seperated without any generic content.
-
-This project is to practice and learn typescript mainly for angular and it will focus on topics required for angular.
-
-hello
-
-how are you?
-
-are you well?
-
-hope you are
-
-that's nice to know
-
-hope this work now
-
-and it should
-
-this is just for github contributions
-
-*/
